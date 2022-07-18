b27b194b73819f45412e7f6f404afef16e2ebb97
romeroc <jorgeromeroc.12@outlook.com>
2022-07-17 23:08:13 -0400

    Enlasando el menu principal con sus respectivas vistas

--- Views/Template/nav.php
+++ Views/Template/nav.php
@@ -32,8 +32,14 @@
         <!-- Sidebar Menu -->
         <nav class="mt-2">
             <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-                <!-- Add icons to the links using the .nav-icon class
-                     with font-awesome or any other icon font library -->
+                <li class="nav-item">
+                    <a href="<?php echo base_url();?>/Dashboard" class="nav-link">
+                        <i class="nav-icon fas fa-home"></i>
+                        <p>
+                            Dashboard
+                        </p>
+                    </a>
+                </li>
                 <li class="nav-item">
                     <a href="#" class="nav-link">
                         <i class="nav-icon fas fa-tachometer-alt"></i>
@@ -44,13 +50,13 @@
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="../index.html" class="nav-link">
+                            <a href="<?php echo base_url();?>/Usuarios" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Usuarios</p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="../index2.html" class="nav-link">
+                            <a href="<?php echo base_url();?>/Categorias" class="nav-link">
                                 <i class="far fa-circle nav-icon"></i>
                                 <p>Categorias</p>
                             </a>
